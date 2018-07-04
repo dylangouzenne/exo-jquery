bee4f0c1bc1304a42d4abda005a166a0e996216b
dylangouzenne <dylan.gouzenne@gmail.com>
2018-07-04 14:33:23 +0200

    afficher données tableau associatif sous forme de liste dans html

---
+++ jquery-basic-list/app.js
@@ -0,0 +1,40 @@
+var books = [
+{
+  title: 'CSS: The Definitive Guide',
+  author: 'Eric Meyer',
+  link: 'http://shop.oreilly.com/product/0636920012726.do',
+  type: 'css'
+},
+{
+  title: 'CSS Development with CSS3',
+  author: 'Zachary Kingston',
+  link: 'http://shop.oreilly.com/product/0636920057970.do',
+  type: 'css'
+},
+{
+  title: 'You Don\'t Know JS: Up & Going',
+  author: 'Kyle Simpson',
+  link: 'http://shop.oreilly.com/product/0636920039303.do',
+  type: 'js'
+},
+{
+  title: 'Programming JavaScript Applications',
+  author: 'Eric Elliott',
+  link: 'http://shop.oreilly.com/product/0636920033141.do',
+  type: 'js'
+},
+{
+  title: 'Modern JavaScript',
+  author: 'unknown',
+  link: 'http://www.oreilly.com/web-platform/free/modern-javascript.csp',
+  type: 'js'
+}
+] ;
+// var titre=books["title"];
+// var auteur=books["author"];
+// var lien=books["link"];
+// var langage=books["type"];
+
+for (var i =0; i<books.length; i++) {
+  $("#tableau").append("<ul><li>"+books[i].title+"</li><li>"+books[i].author+"</li><li>"+books[i].link+"</li><li>"+books[i].type+"</li></ul>");
+}
